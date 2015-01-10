9e064566f696fd42f6a4b6db7a41ad9a831d9b2f
kiptonk <kiptonk@gmail.com>
2015-01-09 18:48:17 -0800

    Initial version

---
+++ test_blinky/src/test_blinky.c
@@ -0,0 +1,50 @@
+/*
+===============================================================================
+ Name        : test_blinky.c
+ Author      : $(author)
+ Version     :
+ Copyright   : $(copyright)
+ Description : main definition
+===============================================================================
+*/
+
+#if defined (__USE_LPCOPEN)
+#if defined(NO_BOARD_LIB)
+#include "chip.h"
+#else
+#include "board.h"
+#endif
+#endif
+
+#include <cr_section_macros.h>
+
+void SysTick_Handler(void)
+{
+	static int x = 0;
+	if (x++ > 500) {
+		Board_LED_Toggle(1);
+		x = 0;
+	}
+}
+
+int main(void) {
+
+#if defined (__USE_LPCOPEN)
+#if !defined(NO_BOARD_LIB)
+    // Read clock settings and update SystemCoreClock variable
+    SystemCoreClockUpdate();
+    // Set up and initialize all required blocks and
+    // functions related to the board hardware
+    Board_Init();
+    // Set the LED to the state of "On"
+#endif
+#endif
+
+	SysTick_Config(SystemCoreClock / 1000);
+
+	while (1) {
+		__WFI();
+	}
+
+    return 0 ;
+}
