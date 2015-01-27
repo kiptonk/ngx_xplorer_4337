39fd76ca2b50cc6050ce3e53065cc2c1b1ab8ca7
kiptonk <kiptonk@gmail.com>
2015-01-27 09:43:16 -0800

    Removed boilerplate

--- test_blinky/src/test_blinky.c
+++ test_blinky/src/test_blinky.c
@@ -1,12 +1,5 @@
-#if defined (__USE_LPCOPEN)
-#if defined(NO_BOARD_LIB)
 #include "chip.h"
-#else
 #include "board.h"
-#endif
-#endif
-
-#include <cr_section_macros.h>
 
 void SysTick_Handler(void)
 {
@@ -18,17 +11,8 @@ void SysTick_Handler(void)
 }
 
 int main(void) {
-
-#if defined (__USE_LPCOPEN)
-#if !defined(NO_BOARD_LIB)
-    // Read clock settings and update SystemCoreClock variable
     SystemCoreClockUpdate();
-    // Set up and initialize all required blocks and
-    // functions related to the board hardware
     Board_Init();
-    // Set the LED to the state of "On"
-#endif
-#endif
 
 	SysTick_Config(SystemCoreClock / 1000);
 
